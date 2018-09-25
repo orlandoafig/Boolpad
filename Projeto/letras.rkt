2da9850ba79f8a5e99a1b4619cf4caae2fa96129
zizao <zizaounesprc@gmail.com>
2018-09-25 12:38:01 -0300

    Arrumando caminho das imagens

--- Projeto/letras.rkt
+++ Projeto/letras.rkt
@@ -11,32 +11,32 @@
 ;; Definindo as letras e operações a serem usadas
 
 (define A (make-object image-snip%
-            "C:/Users/Daniel/Documents/TCC/Projeto/imagens/LetraA.png"
+            "imagens/LetraA.png"
             'png))
 
 (define B (make-object image-snip%
-            "C:/Users/Daniel/Documents/TCC/Projeto/imagens/LetraB.png"
+            "imagens/LetraB.png"
             'png))
 
 (define C (make-object image-snip%
-            "C:/Users/Daniel/Documents/TCC/Projeto/imagens/LetraC.png"
+            "imagens/LetraC.png"
             'png))
 
 (define D (make-object image-snip%
-            "C:/Users/Daniel/Documents/TCC/Projeto/imagens/LetraD.png"
+            "imagens/LetraD.png"
             'png))
 
 (define ou (make-object image-snip%
-             "C:/Users/Daniel/Documents/TCC/Projeto/imagens/OperadorOR.png"
+             "imagens/OperadorOR.png"
              'png))
 
 
 ;------------------------------------ Usando games/cards -------------------------------------;
 
-(define a-bit (make-object bitmap% "C:/Users/Daniel/Documents/TCC/Projeto/imagens/LetraA.png" 'png))
+(define a-bit (make-object bitmap% "imagens/LetraA.png" 'png))
 (define a-card (make-card a-bit #f 1 1))
 
-(define b-bit (make-object bitmap% "C:/Users/Daniel/Documents/TCC/Projeto/imagens/LetraB.png" 'png))
+(define b-bit (make-object bitmap% "imagens/LetraB.png" 'png))
 (define b-card (make-card b-bit #f 1 1))
 
 
