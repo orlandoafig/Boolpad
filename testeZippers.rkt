5cbad269df97540b8a8f4ada5b5744d7ba8a86e8
zizao <zizaounesprc@gmail.com>
2018-10-30 21:44:12 -0300

    testes

--- testeZippers.rkt
+++ testeZippers.rkt
@@ -1,3 +1,12 @@
 #lang racket
 
-(require zippers)
+(require zippers
+         "Projeto/regras.rkt")
+
+(define expr (zip '("ou" "A" "B")))
+
+(define expr2 (zip '("ou" "A" ("e" "A" "B"))))
+
+(define operador (down/car expr))
+
+(define (2cdr exp) (down/cdr (down/cdr exp)))
